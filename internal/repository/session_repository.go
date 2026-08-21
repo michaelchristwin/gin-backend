1e89953c5944dd78c0a2454fb8053794c1f7456b
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-08-21 18:18:31 +0100

    feat: Added gitattributes

--- internal/repository/session_repository.go
+++ internal/repository/session_repository.go
@@ -1,10 +1,23 @@
 package repository
 
+import (
+	"context"
+	sqlc "gin-backend/internal/db"
+)
+
 type SessionRepository interface {
-	Create()
+	Create(ctx context.Context)
 	Get()
 	Delete()
 	DeleteUserSessions()
 	DeleteExpiredSessions()
 	GetSessionWithUser()
 }
+
+type sessionRepository struct {
+	q *sqlc.Queries
+}
+
+// func NewSessionRepository(q *sqlc.Queries) SessionRepository {
+// 	return &sessionRepository{q: q}
+// }
